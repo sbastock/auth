4985a04f115a01e54a51ad4f16191b1549fe93f4
Jane1016 <zjunwh@cn.ibm.com>
2019-12-15 19:23:43 +0800

    first

---
+++ src/main/java/com/stocksba/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.stocksba.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
